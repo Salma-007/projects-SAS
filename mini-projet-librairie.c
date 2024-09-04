74b273baad1551ad343b28f20e5bcfce6e9f130a
Salma-007 <salmaelallali3@gmail.com>
2024-09-04 16:41:55 +0200

    function delete in progress

--- mini-projet-librairie.c
+++ mini-projet-librairie.c
@@ -41,6 +41,15 @@ int main() {
             case 2:
                 afficher_livre();
                 break;
+            case 3:
+                rechercher_livre();
+                break;
+            case 4:
+                update_livre();
+                break;    
+            case 5:
+                delete_livre();
+                break;    
             case 0:
                 printf("Au revoir !\n");
                 break;
@@ -80,17 +89,72 @@ int ajouter_livre() {
 int afficher_livre() {
     if (nombre_livre == 0) {
         printf("Le stock est vide !\n");
-        return;
+        return 0;
     }
     for (int i = 0; i < nombre_livre; i++) {
         printf("Livre %d \n", i + 1);
         printf("Le titre est : %s \n", stock[i].titre);
         printf("L'auteur est : %s \n", stock[i].auteur);
         printf("Le prix est : %.2f \n", stock[i].prix);
-        printf("La quantité est : %d \n", stock[i].quantite);
+        printf("La quantité est : %d \n\n", stock[i].quantite);
     }
 }
 
 int rechercher_livre(){
+    char search[20];
+    printf("tapez le titre du livre :\n");
+    fgets(search, sizeof(search), stdin);
+    strtok(search, "\n"); 
+    for (int i=0; i<nombre_livre ;i++){
+        if(strcmp(search, stock[i].titre) == 0){
+            printf("Le titre est : %s \n", stock[i].titre);
+            printf("L'auteur est : %s \n", stock[i].auteur);
+            printf("Le prix est : %.2f \n", stock[i].prix);
+            printf("La quantité est : %d \n\n", stock[i].quantite);
+            return 1;
+        }
+    }    
+            printf("livre ne se trouve pas!\n");
+            return 0;
+}
+
+int update_livre(){
+    char search[20];
+    printf("tapez le titre du livre: \n");
+    fgets(search, sizeof(search), stdin);
+    strtok(search, "\n");
+     for (int i=0; i<nombre_livre ;i++){
+        if(strcmp(search, stock[i].titre) == 0){
+            printf("livre trouve! \n");
+            printf("donnez la nouvelle quantite svp :\n");
+            scanf("%d",&stock[i].quantite);
+            printf("livre updated avec succes! \n");
+            return 0;
+        }
+   }
+   printf("livre non trouve !\n");
     
 }
+
+int delete_livre(){
+    char title[20];
+    int a;
+    printf("tapez le titre du livre que vous voulez supprimer :\n");
+    fgets(title, sizeof(title), stdin);
+    strtok(title, "\n");
+    for(int i=0; i < nombre_livre; i++){
+        if(strcmp(title , stock[i].titre)==0){
+            printf("est-ce que vous etes sur! \n ");
+            printf("1 : OUI \n ");
+            printf("2 : NON \n ");
+            scanf("%d",&a);
+            if(a==1){
+                for (int j=i; j<nombre_livre ;j++){
+                    stock[j]=stock[j+1];
+                }
+                printf("livre suprime! \n");
+            }}
+            
+        
+        }
+    }
